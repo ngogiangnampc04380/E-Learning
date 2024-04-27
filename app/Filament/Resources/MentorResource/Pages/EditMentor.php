9660a2fb706390b15b3a1ed8c73e782d147cec42
ngogiangnampc04380 <119033482+ngogiangnampc04380@users.noreply.github.com>
2024-04-27 14:46:57 +0700

    [PHP] - mentor

---
+++ app/Filament/Resources/MentorResource/Pages/EditMentor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\MentorResource\Pages;
+
+use App\Filament\Resources\MentorResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditMentor extends EditRecord
+{
+    protected static string $resource = MentorResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
